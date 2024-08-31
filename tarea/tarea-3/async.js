1854963745bd6fef554a64ddbe33d29e460001f5
AlejandroRodriguez <usuariolg80@gmail.com>
2024-08-30 22:10:36 -0500

    subo tarea 3

---
+++ tarea/tarea-3/async.js
@@ -0,0 +1,11 @@
+const esperarSegundos = (tiempo) => {
+    setTimeout(() => {
+    
+        console.log(`El exito no se da de la noche a la mañana. Es cuando cada dia 
+         eres un poco mejor que el dia anterior.Todo suma y han pasado ${tiempo} milesimas de segundo`)
+    }, tiempo); 
+}
+
+
+export default esperarSegundos;
+
---
+++ tarea/tarea-3/init.js
@@ -0,0 +1,5 @@
+import esperarSegundos from './async.js'; // Aca debes poner la ruta donde creaste el archivo async.js
+
+esperarSegundos(2000);
+esperarSegundos(5000);
+esperarSegundos(9000);
