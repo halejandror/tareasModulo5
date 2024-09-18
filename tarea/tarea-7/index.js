5e0f2b4c58392b8f559e874d60a7a28f2a5c279c
AlejandroRodriguez <usuariolg80@gmail.com>
2024-09-17 20:57:41 -0500

    subo actualizacion de tarea 7

--- tarea/tarea-7/index.js
+++ tarea/tarea-7/index.js
@@ -1,28 +1,17 @@
-import express from "express"; // necesario para uso de express / tambien npm i express al inicio de all
+import express, { json } from "express"; // necesario para uso de express / tambien npm i express al inicio de all
+import { readFile, appendFile, open, writeFile, write, unlink, read } from "fs";
+
 
 const app = express(); //aca se iguala una const a express 
 const port = 3002; // aca creamos una const que sera == a nuestro puerto
 
-let students = [
-  { id: 1, name: "Alice", age: 20, major: "Computer Science" },
-  { id: 2, name: "Bob", age: 22, major: "Mathematics" },
-  { id: 3, name: "Charlie", age: 21, major: "Physics" },
-  { id: 4, name: "David", age: 23, major: "Chemistry" },
-  { id: 5, name: "Eve", age: 21, major: "Biology" },
-  { id: 6, name: "Frank", age: 22, major: "Engineering" },
-  { id: 7, name: "Grace", age: 20, major: "Economics" },
-  { id: 8, name: "Hank", age: 23, major: "History" },
-  { id: 9, name: "Ivy", age: 21, major: "Philosophy" },
-  { id: 10, name: "Jack", age: 22, major: "Political Science" },
-  { id: 11, name: "Kara", age: 20, major: "Sociology" },
-  { id: 12, name: "Leo", age: 23, major: "Art" },
-  { id: 13, name: "Mia", age: 21, major: "Music" },
-  { id: 14, name: "Nina", age: 22, major: "Literature" },
-  { id: 15, name: "Oscar", age: 20, major: "Psychology" },
-  { id: 16, name: "Paul", age: 23, major: "Geography" },
-  { id: 17, name: "Quinn", age: 21, major: "Anthropology" },
-  { id: 18, name: "Rose", age: 22, major: "Linguistics" },
-];
+let students= readFile('studentsArchivo.json' , "utf8", (error, data )=> {
+  if(error)console.error(error);
+  else{
+    students= (JSON.parse(data))
+  }
+}) ;
+
 // linea 27 a 29 muestro el array de estudiantes en tipo JSON
 app.get("/students", (req, res) => {
   res.send(students);
