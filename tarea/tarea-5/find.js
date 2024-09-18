e9d5ce339e1f7d6211fc8a73013f98d3e9d36012
AlejandroRodriguez <usuariolg80@gmail.com>
2024-09-17 22:03:37 -0500

    cambios en tarea 5

--- tarea/tarea-5/find.js
+++ tarea/tarea-5/find.js
@@ -1,17 +1,17 @@
-import { createReadStream } from "fs";
 import { readFile, appendFile, open, writeFile, write, unlink } from "fs";
 
-readFile("numeros.txt", 'utf-8',(error, data) => {
+readFile("numeros.txt", "utf-8", (error, data) => {
   if (error) console.error(error);
-  else console.log([data.toString()]);
+  else console.log([data.toString()]); //to.String convierte de buffer a String
 
-  const numeros= data.split(',') // con esto convertimos el String en un array
-  const pares= numeros.map(num => parseInt(num.trim())).filter(num => num %2 ===0) //.map y parseInt(num.trim()) esta convirtiendo a numeros y filter nos devuelve solo pares 
+  const numeros = data.split(","); // con esto convertimos el String en un array
+  const pares = numeros
+    .map((num) => parseInt(num.trim()))//.map y parseInt(num.trim()) esta convirtiendo a numeros
+    .filter((num) => num % 2 === 0);   //filter nos devuelve solo los q al dividir por 2 de 0
   console.log(pares);
 
-  writeFile('numerosPares.txt', `${pares}`, (error) => {
-      if(error) console.log(error)
-      else console.log("file updated")
-  })
-
+  writeFile("numerosPares.txt", `${pares}`, (error) => {
+    if (error) console.log(error);
+    else console.log("file updated");
+  });
 });
